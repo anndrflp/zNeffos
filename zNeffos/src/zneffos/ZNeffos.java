ea05f4cb8fb21881abd7aaf1269a83ab2287b2da
Anderson Felipe <30373732+anndrflp@users.noreply.github.com>
2018-03-23 21:38:54 -0300

    Adicionando a conexão com o MySql

--- zNeffos/src/zneffos/ZNeffos.java
+++ zNeffos/src/zneffos/ZNeffos.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package zneffos;
 
+package zneffos;
+import DAO.ConexaoMySQL;
 /**
  *
  * @author Usuário
@@ -17,6 +13,10 @@ public class ZNeffos {
     public static void main(String[] args) {
         // TODO code application logic here]
         System.out.println("Teste");
+        
+        ConexaoMySQL conn = new ConexaoMySQL();
+        
+        conn.getConexaoMySQL();
     }
     
 }
